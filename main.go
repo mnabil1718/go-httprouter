3dae625e915c329b6c7585bf6b13b4b2505048c7
tingkatin <muhammad.nabil@superindo.co.id>
2024-05-27 12:53:48 +0700

    Added httprouter PanicHandler at main.go

--- main.go
+++ main.go
@@ -9,8 +9,20 @@ import (
 
 func main() {
 	router := httprouter.New()
+
+	router.PanicHandler = func(writer http.ResponseWriter, request *http.Request, error interface{}) {
+		message, ok := error.(string) // type assertion to string
+		if ok {
+			fmt.Fprintf(writer, "Error: %s", message)
+		}
+	}
+
 	router.GET("/", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 		fmt.Fprint(writer, "Hello world")
+	})
+
+	router.GET("/panic-example", func(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+		panic("Intentional Server Error")
 	})
 
 	server := http.Server{
